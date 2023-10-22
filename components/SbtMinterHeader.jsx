e9fc0c4237b77be6732289db12b7529c17c401c4
0xTemplar <ademolaadebowale74@gmail.com>
2023-10-22 03:09:06 +0100

    complete standard domain minter functions

--- components/SbtMinterHeader.jsx
+++ components/SbtMinterHeader.jsx
@@ -180,7 +180,7 @@ const SbtMinter = () => {
         {!isConnected && (
           <button
             type="submit"
-            className="text-white font-bold border-2 border-[button-gradient] flex mx-auto justify-center bg-slate-600 hover:opacity-80 focus:ring-4 focus:outline-none focus:ring-blue-300  rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-centerk"
+            className="text-white font-bold border-2 border-[button-gradient] flex mx-auto justify-center bg-slate-600 hover:opacity-80 focus:ring-4 focus:outline-none focus:ring-blue-300  rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center"
             onClick={notify}
           >
             Mint
